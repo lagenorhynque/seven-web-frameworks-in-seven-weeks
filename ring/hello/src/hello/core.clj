c1b5b44c70426a04a766064bb0b98104c70ac954
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2017-11-26 23:49:08 +0900

    implement "Rumble" app
    - ring/hello: add default route

--- ring/hello/src/hello/core.clj
+++ ring/hello/src/hello/core.clj
@@ -1,6 +1,8 @@
 (ns hello.core
-  (:require [compojure.core :refer [GET defroutes]]))
+  (:require [compojure.core :refer [GET defroutes]]
+            [compojure.route :as route]))
 
 (defroutes app
   (GET "/" []
-       "Hello, world!"))
+       "Hello, world!")
+  (route/not-found "Not Found"))
---
+++ yesod/rumble/app/devel.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE PackageImports #-}
+import "rumble" Application (develMain)
+import Prelude (IO)
+
+main :: IO ()
+main = develMain
